d3342af56b720aa69e9c60bffeb06901a09f0eaf
Sean Feldman <feldman.sean@gmail.com>
2015-12-14 10:58:22 -0700

    Added ATTs

---
+++ src/NServiceBus.AzureStoragePersistence.AcceptanceTests/ConfigureAzureStoragePersistence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Persistence;
+using NServiceBus.SagaPersisters;
+
+public class ConfigureAzureStoragePersistence
+{
+    public void Configure(BusConfiguration config)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("AzureStoragePersistence.ConnectionString");
+        config.UsePersistence<AzureStoragePersistence, StorageType.Subscriptions>().ConnectionString(connectionString);
+        config.UsePersistence<AzureStoragePersistence, StorageType.Sagas>().ConnectionString(connectionString);
+        config.UsePersistence<AzureStoragePersistence, StorageType.Timeouts>().ConnectionString(connectionString);
+    }
+
+    public void Cleanup()
+    {
+    }
+}
