31a6b7f6de32cbeec4ca19af019099ca24879635
Scooletz <scooletz@gmail.com>
2016-03-14 15:23:52 +0100

    Secondary indices have now IndexDefintion covered with tests.

---
+++ src/NServiceBus.AzureStoragePersistence.ComponentTests/Sagas/IndexDefinitionTests.cs
@@ -0,0 +1,52 @@
+﻿namespace NServiceBus.AzureStoragePersistence.ComponentTests.Sagas
+{
+    using System;
+    using Saga;
+    using SagaPersisters.Azure.SecondaryIndeces;
+    using NUnit.Framework;
+
+    public class IndexDefinitionTests
+    {
+        readonly IndexDefintion index;
+
+        private class SagaData : ContainSagaData
+        {
+            [Unique]
+            public string AdditionalId { get; set; }
+        }
+
+        public IndexDefinitionTests()
+        {
+            index = IndexDefintion.Get(typeof(SagaData));
+        }
+
+        [Test]
+        public void Should_access_value_properly()
+        {
+            const string id = "FF4E1C4E-D2F2-4601-8D8E-CB3E91872043";
+            var sagaData = new SagaData
+            {
+                AdditionalId = id
+            };
+
+            Assert.AreEqual(id, index.Accessor(sagaData));
+        }
+
+        [Test]
+        public void Should_validate_property()
+        {
+            Assert.Throws<ArgumentException>(() => index.ValidateProperty("AdditionalId_"));
+
+            index.ValidateProperty("AdditionalId");
+        }
+
+        [Test]
+        public void Should_build_index_key()
+        {
+            const string id = "C4D91B59-A407-4CDA-A689-60AA3C334699";
+            var key = index.BuildTableKey(id);
+            Assert.AreEqual("Index_NServiceBus.AzureStoragePersistence.ComponentTests.Sagas.IndexDefinitionTests+SagaData_AdditionalId_\"C4D91B59-A407-4CDA-A689-60AA3C334699\"", key.PartitionKey);
+            Assert.AreEqual("", key.RowKey);
+        }
+    }
+}
--- src/NServiceBus.AzureStoragePersistence/SagaPersisters/Azure/SecondaryIndeces/SecondaryIndexPersister.cs
+++ src/NServiceBus.AzureStoragePersistence/SagaPersisters/Azure/SecondaryIndeces/SecondaryIndexPersister.cs
@@ -1,14 +1,9 @@
 ﻿namespace NServiceBus.SagaPersisters.Azure.SecondaryIndeces
 {
     using System;
-    using System.Collections.Concurrent;
-    using System.IO;
-    using System.Linq.Expressions;
     using System.Net;
-    using System.Reflection;
     using Microsoft.WindowsAzure.Storage;
     using Microsoft.WindowsAzure.Storage.Table;
-    using Newtonsoft.Json;
     using NServiceBus.Saga;
 
     public class SecondaryIndexPersister
@@ -144,71 +139,6 @@
         private static bool IsConflict(StorageException ex)
         {
             return ex.RequestInformation.HttpStatusCode == (int) HttpStatusCode.Conflict;
-        }
-
-        private class IndexDefintion
-        {
-            static readonly ConcurrentDictionary<Type, object> sagaToIndex = new ConcurrentDictionary<Type, object>();
-            static readonly object NullValue = new object();
-
-            static readonly ParameterExpression ObjectParameter = Expression.Parameter(typeof(object));
-            readonly string propertyName;
-
-            readonly string sagaTypeName;
-
-            private IndexDefintion(Type sagaType, PropertyInfo pi)
-            {
-                sagaTypeName = sagaType.FullName;
-                propertyName = pi.Name;
-                Accessor = Expression.Lambda<Func<object, object>>(Expression.Convert(Expression.MakeMemberAccess(Expression.Convert(ObjectParameter, sagaType), pi), typeof(object)), ObjectParameter).Compile();
-            }
-
-            public Func<object, object> Accessor { get; }
-
-            public static IndexDefintion Get(Type sagaType)
-            {
-                var index = sagaToIndex.GetOrAdd(sagaType, type =>
-                {
-                    var pi = UniqueAttribute.GetUniqueProperty(sagaType);
-                    if (pi == null)
-                    {
-                        return NullValue;
-                    }
-
-                    return new IndexDefintion(sagaType, pi);
-                });
-
-                if (ReferenceEquals(index, NullValue))
-                {
-                    return null;
-                }
-
-                return (IndexDefintion) index;
-            }
-
-            public void ValidateProperty(string propertyName)
-            {
-                if (this.propertyName != propertyName)
-                {
-                    throw new ArgumentException($"The following saga '{sagaTypeName}' is not indexed by '{propertyName}'. The only secondary index is defined for '{this.propertyName}'. " +
-                                                $"Ensure that the saga is correlated properly.");
-                }
-            }
-
-            public PartitionRowKeyTuple BuildTableKey(object propertyValue)
-            {
-                return new PartitionRowKeyTuple($"Index_{sagaTypeName}_{propertyName}_{Serialize(propertyValue)}", "");
-            }
-
-            private static string Serialize(object propertyValue)
-            {
-                using (var sw = new StringWriter())
-                {
-                    new JsonSerializer().Serialize(sw, propertyValue);
-                    sw.Flush();
-                    return sw.ToString();
-                }
-            }
         }
     }
 }
