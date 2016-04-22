80f3190c0e049d0a2df036ce0901d54aaa839cab
Kyle Baley <kyle@baley.org>
2016-04-22 11:06:07 -0400

    Fix namespace inspection error

--- src/NServiceBus.AzureStoragePersistence/SagaPersisters/AzureSagaPersister.cs
+++ src/NServiceBus.AzureStoragePersistence/SagaPersisters/AzureSagaPersister.cs
@@ -10,7 +10,7 @@
     using System.Threading.Tasks;
     using Microsoft.WindowsAzure.Storage;
     using Microsoft.WindowsAzure.Storage.Table;
-    using NServiceBus.Azure;
+    using Azure;
     using Extensibility;
     using Persistence;
     using SecondaryIndices;
