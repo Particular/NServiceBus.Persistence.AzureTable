cecde17ef218817816cc5ac2ee58b6147a5774a0
Tim Bussmann <tim.bussmann@particular.net>
2016-09-14 12:17:09 +0200

    add test to validate expected subscription storage behavior

---
+++ src/NServiceBus.Persistence.AzureStorage.ComponentTests/Subscriptions/When_updating_subscription.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.Persistence.AzureStorage.ComponentTests.Subscriptions
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Unicast.Subscriptions;
+    using Unicast.Subscriptions.MessageDrivenSubscriptions;
+
+    [TestFixture]
+    [Category("AzureStoragePersistence")]
+    public class When_updating_subscription
+    {
+        [SetUp]
+        public void Setup()
+        {
+            SubscriptionTestHelper.PerformStorageCleanup();
+        }
+
+        [Test]
+        public async Task new_subscription_should_overwrite_existing()
+        {
+            var persister = SubscriptionTestHelper.CreateAzureSubscriptionStorage();
+            var messageType = new MessageType(typeof(TestMessage));
+
+            await persister.Subscribe(new Subscriber("address://test-queue", null), messageType, null);
+            await persister.Subscribe(new Subscriber("address://test-queue", "1"), messageType, null);
+            await persister.Subscribe(new Subscriber("address://test-queue", "2"), messageType, null);
+
+            var subscribers = (await persister.GetSubscriberAddressesForMessage(new[]
+            {
+                messageType
+            }, null)).ToArray();
+
+            Assert.AreEqual(1, subscribers.Length);
+            Assert.AreEqual("address://test-queue", subscribers[0].TransportAddress);
+            Assert.AreEqual("2", subscribers[0].Endpoint);
+        }
+    }
+}
