88d1b2b3792b1b179ecab92e76fb1e11f4da25bd
Szymon Kulec <scooletz@gmail.com>
2016-11-16 12:49:14 +0100

    Revert "Non-primitive properties now are serialized with JSON instead of throwing an error. (#99)"
    This reverts commit c0aa235fc775ee7ea7fa51b906165806014670d8.

--- src/NServiceBus.Persistence.AzureStorage/SagaPersisters/DictionaryTableEntityExtensions.cs
+++ src/NServiceBus.Persistence.AzureStorage/SagaPersisters/DictionaryTableEntityExtensions.cs
@@ -2,17 +2,14 @@ namespace NServiceBus.Persistence.AzureStorage
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Reflection;
     using Microsoft.WindowsAzure.Storage.Table;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Serialization;
 
     static class DictionaryTableEntityExtensions
     {
         public static TEntity ToEntity<TEntity>(DictionaryTableEntity entity)
         {
-            return (TEntity)ToEntity(typeof(TEntity), entity);
+            return (TEntity) ToEntity(typeof(TEntity), entity);
         }
 
         public static object ToEntity(Type entityType, DictionaryTableEntity entity)
@@ -27,64 +24,47 @@ namespace NServiceBus.Persistence.AzureStorage
             {
                 if (entity.ContainsKey(propertyInfo.Name))
                 {
-                    var value = entity[propertyInfo.Name];
                     if (propertyInfo.PropertyType == typeof(byte[]))
                     {
-                        propertyInfo.SetValue(toCreate, value.BinaryValue, null);
+                        propertyInfo.SetValue(toCreate, entity[propertyInfo.Name].BinaryValue, null);
                     }
                     else if (propertyInfo.PropertyType == typeof(bool))
                     {
-                        var boolean = value.BooleanValue;
+                        var boolean = entity[propertyInfo.Name].BooleanValue;
                         propertyInfo.SetValue(toCreate, boolean.HasValue && boolean.Value, null);
                     }
                     else if (propertyInfo.PropertyType == typeof(DateTime))
                     {
-                        var dateTimeOffset = value.DateTimeOffsetValue;
+                        var dateTimeOffset = entity[propertyInfo.Name].DateTimeOffsetValue;
                         propertyInfo.SetValue(toCreate, dateTimeOffset?.DateTime ?? default(DateTime), null);
                     }
                     else if (propertyInfo.PropertyType == typeof(Guid))
                     {
-                        var guid = value.GuidValue;
+                        var guid = entity[propertyInfo.Name].GuidValue;
                         propertyInfo.SetValue(toCreate, guid ?? default(Guid), null);
                     }
                     else if (propertyInfo.PropertyType == typeof(int))
                     {
-                        var int32 = value.Int32Value;
+                        var int32 = entity[propertyInfo.Name].Int32Value;
                         propertyInfo.SetValue(toCreate, int32 ?? default(int), null);
                     }
                     else if (propertyInfo.PropertyType == typeof(double))
                     {
-                        var d = value.DoubleValue;
+                        var d = entity[propertyInfo.Name].DoubleValue;
                         propertyInfo.SetValue(toCreate, d ?? default(long), null);
                     }
                     else if (propertyInfo.PropertyType == typeof(long))
                     {
-                        var int64 = value.Int64Value;
+                        var int64 = entity[propertyInfo.Name].Int64Value;
                         propertyInfo.SetValue(toCreate, int64 ?? default(long), null);
                     }
                     else if (propertyInfo.PropertyType == typeof(string))
                     {
-                        propertyInfo.SetValue(toCreate, value.StringValue, null);
+                        propertyInfo.SetValue(toCreate, entity[propertyInfo.Name].StringValue, null);
                     }
                     else
                     {
-                        if (value.PropertyType == EdmType.String)
-                        {
-                            // possibly serialized JSON.NET value
-                            try
-                            {
-                                var deserialized = JsonSerializer.Create().Deserialize(new StringReader(value.StringValue), propertyInfo.PropertyType);
-                                propertyInfo.SetValue(toCreate, deserialized, null);
-                            }
-                            catch (Exception)
-                            {
-                                throw new NotSupportedException($"The property type '{propertyInfo.PropertyType.Name}' is not supported in windows azure table storage neither it can be deserialized with JSON.NET.");
-                            }
-                        }
-                        else
-                        {
-                            throw new NotSupportedException($"The property type '{propertyInfo.PropertyType.Name}' is not supported in windows azure table storage");
-                        }
+                        throw new NotSupportedException($"The property type '{propertyInfo.PropertyType.Name}' is not supported in windows azure table storage");
                     }
                 }
             }
@@ -97,52 +77,39 @@ namespace NServiceBus.Persistence.AzureStorage
             {
                 if (propertyInfo.PropertyType == typeof(byte[]))
                 {
-                    toPersist[propertyInfo.Name] = new EntityProperty((byte[])propertyInfo.GetValue(entity, null));
+                    toPersist[propertyInfo.Name] = new EntityProperty((byte[]) propertyInfo.GetValue(entity, null));
                 }
                 else if (propertyInfo.PropertyType == typeof(bool))
                 {
-                    toPersist[propertyInfo.Name] = new EntityProperty((bool)propertyInfo.GetValue(entity, null));
+                    toPersist[propertyInfo.Name] = new EntityProperty((bool) propertyInfo.GetValue(entity, null));
                 }
                 else if (propertyInfo.PropertyType == typeof(DateTime))
                 {
-                    toPersist[propertyInfo.Name] = new EntityProperty((DateTime)propertyInfo.GetValue(entity, null));
+                    toPersist[propertyInfo.Name] = new EntityProperty((DateTime) propertyInfo.GetValue(entity, null));
                 }
                 else if (propertyInfo.PropertyType == typeof(Guid))
                 {
-                    toPersist[propertyInfo.Name] = new EntityProperty((Guid)propertyInfo.GetValue(entity, null));
+                    toPersist[propertyInfo.Name] = new EntityProperty((Guid) propertyInfo.GetValue(entity, null));
                 }
                 else if (propertyInfo.PropertyType == typeof(int))
                 {
-                    toPersist[propertyInfo.Name] = new EntityProperty((int)propertyInfo.GetValue(entity, null));
+                    toPersist[propertyInfo.Name] = new EntityProperty((int) propertyInfo.GetValue(entity, null));
                 }
                 else if (propertyInfo.PropertyType == typeof(long))
                 {
-                    toPersist[propertyInfo.Name] = new EntityProperty((long)propertyInfo.GetValue(entity, null));
+                    toPersist[propertyInfo.Name] = new EntityProperty((long) propertyInfo.GetValue(entity, null));
                 }
                 else if (propertyInfo.PropertyType == typeof(double))
                 {
-                    toPersist[propertyInfo.Name] = new EntityProperty((double)propertyInfo.GetValue(entity, null));
+                    toPersist[propertyInfo.Name] = new EntityProperty((double) propertyInfo.GetValue(entity, null));
                 }
                 else if (propertyInfo.PropertyType == typeof(string))
                 {
-                    toPersist[propertyInfo.Name] = new EntityProperty((string)propertyInfo.GetValue(entity, null));
+                    toPersist[propertyInfo.Name] = new EntityProperty((string) propertyInfo.GetValue(entity, null));
                 }
                 else
                 {
-                    using (var sw = new StringWriter())
-                    {
-                        try
-                        {
-                            var serializer = JsonSerializer.Create();
-                            serializer.ContractResolver = new NonAbstractDefaultContractResolver();
-                            serializer.Serialize(sw, propertyInfo.GetValue(entity, null), propertyInfo.PropertyType);
-                        }
-                        catch (Exception)
-                        {
-                            throw new NotSupportedException($"The property type '{propertyInfo.PropertyType.Name}' is not supported in windows azure table storage neither it can be serialized with JSON.NET.");
-                        }
-                        toPersist[propertyInfo.Name] = new EntityProperty(sw.ToString());
-                    }
+                    throw new NotSupportedException($"The property type '{propertyInfo.PropertyType.Name}' is not supported in windows azure table storage");
                 }
             }
             return toPersist;
@@ -160,35 +127,35 @@ namespace NServiceBus.Persistence.AzureStorage
 
             if (propertyInfo.PropertyType == typeof(byte[]))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForBinary(property, QueryComparisons.Equal, (byte[])value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForBinary(property, QueryComparisons.Equal, (byte[]) value));
             }
             else if (propertyInfo.PropertyType == typeof(bool))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForBool(property, QueryComparisons.Equal, (bool)value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForBool(property, QueryComparisons.Equal, (bool) value));
             }
             else if (propertyInfo.PropertyType == typeof(DateTime))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForDate(property, QueryComparisons.Equal, (DateTime)value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForDate(property, QueryComparisons.Equal, (DateTime) value));
             }
             else if (propertyInfo.PropertyType == typeof(Guid))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForGuid(property, QueryComparisons.Equal, (Guid)value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForGuid(property, QueryComparisons.Equal, (Guid) value));
             }
             else if (propertyInfo.PropertyType == typeof(int))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForInt(property, QueryComparisons.Equal, (int)value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForInt(property, QueryComparisons.Equal, (int) value));
             }
             else if (propertyInfo.PropertyType == typeof(long))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForLong(property, QueryComparisons.Equal, (long)value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForLong(property, QueryComparisons.Equal, (long) value));
             }
             else if (propertyInfo.PropertyType == typeof(double))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForDouble(property, QueryComparisons.Equal, (double)value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForDouble(property, QueryComparisons.Equal, (double) value));
             }
             else if (propertyInfo.PropertyType == typeof(string))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterCondition(property, QueryComparisons.Equal, (string)value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterCondition(property, QueryComparisons.Equal, (string) value));
             }
             else
             {
@@ -196,21 +163,6 @@ namespace NServiceBus.Persistence.AzureStorage
             }
 
             return query;
-        }
-
-        private class NonAbstractDefaultContractResolver : DefaultContractResolver
-        {
-            public NonAbstractDefaultContractResolver() : base(true)
-            {}
-
-            protected override JsonObjectContract CreateObjectContract(Type objectType)
-            {
-                if (objectType.IsAbstract || objectType.IsInterface)
-                {
-                    throw new ArgumentException("Cannot serialize an abstract class/interface", nameof(objectType));
-                }
-                return base.CreateObjectContract(objectType);
-            }
         }
     }
 }
