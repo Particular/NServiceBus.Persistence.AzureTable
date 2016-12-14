b16b5d7e0750778b1424bd1322b1be93252c7f1e
Tomasz Masternak <tomasz.masternak@gmail.com>
2016-12-14 15:35:35 +0100

    tweaks to GetNextChunk performance test

--- src/NServiceBus.Persistence.AzureStorage.ComponentTests/Timeouts/When_there_is_lots_of_future_timeouts.cs
+++ src/NServiceBus.Persistence.AzureStorage.ComponentTests/Timeouts/When_there_is_lots_of_future_timeouts.cs
@@ -2,9 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Net;
     using System.Threading.Tasks;
     using Extensibility;
     using NUnit.Framework;
+    using NUnit.Framework.Compatibility;
     using Timeout.Core;
 
     [TestFixture]
@@ -19,15 +21,21 @@
             return TestHelper.PerformStorageCleanup();
         }
 
-        [Test]
+        [Test, Explicit("Used to check what is performance of GetNextChunk with considerable number of timeout rows outside of query criteria")]
         public async Task Should_return_timeouts_to_dispatch_reasonably_fast()
         {
+            ServicePointManager.DefaultConnectionLimit = 100;
+            ServicePointManager.UseNagleAlgorithm = false;
+            ServicePointManager.Expect100Continue = false;
+
             var timeoutPersister = TestHelper.CreateTimeoutPersister();
-            
+
             var future = DateTime.UtcNow.AddDays(1);
 
-            const int count = 100;
+            const int count = 1000;
             var tasks = new Task[count];
+
+            var insertStopWatch = Stopwatch.StartNew();
 
             for (var i = 0; i < count; i++)
             {
@@ -38,11 +46,18 @@
             }
 
             await Task.WhenAll(tasks);
+            var insertElapsed = insertStopWatch.ElapsedMilliseconds;
+
+            Console.WriteLine($"Insert took {insertElapsed} ms.");
 
             var outlier = DateTime.UtcNow.AddDays(-1);
             await timeoutPersister.Add(GenerateTimeout(outlier), new ContextBag());
 
+            var stopwatch = Stopwatch.StartNew();
             var peekedTimeout = await timeoutPersister.GetNextChunk(WhateverDate);
+            var elapsed = stopwatch.ElapsedMilliseconds;
+
+            Console.WriteLine($"GetNextChunk took {elapsed} ms.");
 
             Assert.AreEqual(1, peekedTimeout.DueTimeouts.Length);
             Assert.AreEqual(outlier, peekedTimeout.DueTimeouts[0].DueTime);
