e0369cee1cfa50cf65b4d1491ef3b4f74c2c6a86
Tomasz Masternak <tomasz.masternak@gmail.com>
2016-12-16 15:57:15 +0100

    more test - still need more

--- src/NServiceBus.Persistence.AzureStorage.ComponentTests/Timeouts/When_querying_timeouts.cs
+++ src/NServiceBus.Persistence.AzureStorage.ComponentTests/Timeouts/When_querying_timeouts.cs
@@ -25,7 +25,7 @@
         }
 
         [Test]
-        public async Task Returns_all_if_all_are_current_and_within_batch_size()
+        public async Task Returns_all_if_due_timeouts_are_current_and_within_batch_size()
         {
             var now = DateTime.Now;
             var segment1 = new List<TimeoutDataEntity> { CreateCurrentTimeout(now) };
@@ -35,6 +35,33 @@
 
             AssertEqual(segment1, results);
             Assert.AreEqual(now.Add(TimeoutPersister.DefaultNextQueryDelay), results.NextTimeToQuery);
+        }
+
+        [Test]
+        public async Task Returns_all_if_due_timeouts_are_returned_in_two_segments()
+        {
+            var now = DateTime.Now;
+            var segment1 = new List<TimeoutDataEntity> { CreateCurrentTimeout(now) };
+            var segment2 = new List<TimeoutDataEntity> { CreateCurrentTimeout(now) };
+            var executor = CreateQueryExecutor(segment1, segment2);
+
+            var results = await TimeoutPersister.CalculateNextTimeoutChunk(executor, null, now);
+
+            AssertEqual(segment1.Union(segment2), results);
+            Assert.AreEqual(now.Add(TimeoutPersister.DefaultNextQueryDelay), results.NextTimeToQuery);
+        }
+
+        [Test]
+        public async Task Returns_only_due_timeouts_from_single_segment()
+        {
+            var now = DateTime.Now;
+            var segment1 = new List<TimeoutDataEntity> { CreateCurrentTimeout(now), CreateFutrueTimeout(now) };
+            var executor = CreateQueryExecutor(segment1);
+
+            var results = await TimeoutPersister.CalculateNextTimeoutChunk(executor, null, now);
+
+            AssertEqual(segment1.Take(1), results);
+            Assert.AreEqual(segment1[1].Time, results.NextTimeToQuery);
         }
 
         static void AssertEqual(IEnumerable<TimeoutDataEntity> segment1, TimeoutsChunk results)
@@ -47,6 +74,13 @@
             return new TimeoutDataEntity { Time = now.AddSeconds(-1), RowKey = Guid.NewGuid().ToString()};
         }
 
+        static TimeoutDataEntity CreateFutrueTimeout(DateTime now)
+        {
+            return new TimeoutDataEntity { Time = now.AddMinutes(10), RowKey = Guid.NewGuid().ToString()};
+        }
+
+
+
         static Func<TableQuery<TimeoutDataEntity>, TableContinuationToken, Task<TableQuerySegment<TimeoutDataEntity>>> CreateQueryExecutor(params List<TimeoutDataEntity>[] values)
         {
             var counter = 0;
@@ -57,9 +91,11 @@
                     throw new Exception("Too many calls to provider");
                 }
 
-                var results = values[counter++];
+                var results = values[counter];
+
                 counter += 1;
-                return Task.FromResult(CreateSegment(results, null));
+
+                return Task.FromResult(CreateSegment(results, counter >= values.Length ? null : new TableContinuationToken()));
             };
         }
 
