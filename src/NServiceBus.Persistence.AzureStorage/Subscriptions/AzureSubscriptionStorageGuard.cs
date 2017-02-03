e526aad5a46ad70ea89df162cf1055137f179b64
danielmarbach <daniel.marbach@openplace.net>
2017-02-03 08:50:42 +0100

    Removed unnecessary cast in AzureSubscriptionStorageGuard and made static

--- src/NServiceBus.Persistence.AzureStorage/Subscriptions/AzureSubscriptionStorageGuard.cs
+++ src/NServiceBus.Persistence.AzureStorage/Subscriptions/AzureSubscriptionStorageGuard.cs
@@ -3,25 +3,25 @@
     using System;
     using System.Text.RegularExpressions;
 
-    class AzureSubscriptionStorageGuard
+    static class AzureSubscriptionStorageGuard
     {
-        public static void CheckConnectionString(object connectionString)
+        public static void CheckConnectionString(string connectionString)
         {
-            if (string.IsNullOrWhiteSpace((string)connectionString))
+            if (string.IsNullOrWhiteSpace(connectionString))
             {
-                throw new ArgumentException("ConnectionString should not be an empty string.", "connectionString");
+                throw new ArgumentException("ConnectionString should not be an empty string.", nameof(connectionString));
             }
         }
 
-        public static void CheckTableName(object tableName)
+        public static void CheckTableName(string tableName)
         {
-            if (string.IsNullOrWhiteSpace((string)tableName))
+            if (string.IsNullOrWhiteSpace(tableName))
             {
-                throw new ArgumentException("Table name should not be an empty string.", "tableName");
+                throw new ArgumentException("Table name should not be an empty string.", nameof(tableName));
             }
 
             var tableNameRegex = new Regex("^[A-Za-z][A-Za-z0-9]{2,62}(?!tables)$");
-            if (!tableNameRegex.IsMatch((string)tableName))
+            if (!tableNameRegex.IsMatch(tableName))
             {
                 // error message is following MSFT guidelines http://msdn.microsoft.com/library/azure/dd179338.aspx
                 const string errorMessage = "Invalid table name. Valid name should follow these rules:\n"
@@ -29,7 +29,7 @@
                                             + " Cannot begin with a numeric character.\n"
                                             + " Must be from 3 to 63 characters long."
                                             + " Avoid reserved names, such as \"tables\".";
-                throw new ArgumentException(errorMessage, "tableName");
+                throw new ArgumentException(errorMessage, nameof(tableName));
             }
         }
     }
