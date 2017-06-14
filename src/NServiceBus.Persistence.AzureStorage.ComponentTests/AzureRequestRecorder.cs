14da0fe43bbd3ebcee97f71220daa3a7a87a7074
Sean Feldman <feldman.sean@gmail.com>
2017-06-14 12:05:49 -0600

    Merge branch 'test-for-no-table-scan' into release-1.4

---
+++ src/NServiceBus.Persistence.AzureStorage.ComponentTests/AzureRequestRecorder.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.Persistence.AzureStorage.ComponentTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Microsoft.WindowsAzure.Storage;
+
+    public class AzureRequestRecorder : IDisposable
+    {
+        public List<string> Requests = new List<string>();
+
+        public AzureRequestRecorder()
+        {
+            OperationContext.GlobalSendingRequest += OnSendingRequest;
+        }
+
+        void OnSendingRequest(object sender, RequestEventArgs e)
+        {
+            Requests.Add($"{e.Request.Method,-7} {e.Request.RequestUri.PathAndQuery}");
+        }
+
+        public void Dispose()
+        {
+            OperationContext.GlobalSendingRequest -= OnSendingRequest;
+        }
+
+        public void Print(TextWriter @out)
+        {
+            @out.WriteLine("Recorded calls to Azure Storage Services");
+
+            foreach (var request in Requests)
+            {
+                @out.WriteLine($"- {request}");
+            }
+
+            @out.WriteLine();
+        }
+    }
+}
