100fd2b0d61c4e619049d198fcb3d5747f1469a5
Sean Feldman <feldman.sean@gmail.com>
2017-09-22 15:45:01 -0600

    Persistence related unit tests to use correct connection string

--- src/Tests/Sagas/When_updating_saga.cs
+++ src/Tests/Sagas/When_updating_saga.cs
@@ -10,7 +10,7 @@
         [Test]
         public async Task Should_save_updated_properties()
         {
-            var connectionString = Testing.Utillities.GetEnvConfiguredConnectionStringForTransport();
+            var connectionString = Testing.Utillities.GetEnvConfiguredConnectionStringForPersistence();
 
             var persister = new AzureSagaPersister(connectionString, true);
             var originalProp = Guid.NewGuid().ToString();
