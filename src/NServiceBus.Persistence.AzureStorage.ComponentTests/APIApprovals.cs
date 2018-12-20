fd1b90d7278e73dac3850df95f2c32321aea8e95
Brandon Ording <bording@gmail.com>
2018-12-20 13:23:43 -0500

    Switch to Particular.Approvals

--- src/NServiceBus.Persistence.AzureStorage.ComponentTests/APIApprovals.cs
+++ src/NServiceBus.Persistence.AzureStorage.ComponentTests/APIApprovals.cs
@@ -1,17 +1,15 @@
-﻿using System.Runtime.CompilerServices;
-using NServiceBus;
-using NServiceBus.Persistence.AzureStorage.ComponentTests;
+﻿using NServiceBus;
 using NUnit.Framework;
+using Particular.Approvals;
 using PublicApiGenerator;
 
 [TestFixture]
 public class APIApprovals
 {
     [Test]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public void ApproveAzureStorageQueueTransport()
     {
         var publicApi = ApiGenerator.GeneratePublicApi(typeof(AzureStoragePersistence).Assembly);
-        TestApprover.Verify(publicApi);
+        Approver.Verify(publicApi);
     }
 }
