1e972e1fa261705b951bd409a1d32e0c0cad90e8
lailabougria <laila.bougria@particular.net>
2020-07-28 19:04:07 +0200

    Set settings for azure storage

--- src/NServiceBus.Persistence.AzureStorage.PersistenceTests/PersistenceTestsConfiguration.cs
+++ src/NServiceBus.Persistence.AzureStorage.PersistenceTests/PersistenceTestsConfiguration.cs
@@ -5,28 +5,54 @@
     using NServiceBus.Outbox;
     using NServiceBus.Sagas;
     using Persistence;
+    using Persistence.AzureStorage;
 
     public partial class PersistenceTestsConfiguration
     {
-        public bool SupportsDtc { get; }
-        public bool SupportsOutbox { get; }
-        public bool SupportsFinders { get; }
-        public bool SupportsSubscriptions { get; }
-        public bool SupportsTimeouts { get; }
-        public bool SupportsPessimisticConcurrency { get; }
-        public ISagaIdGenerator SagaIdGenerator { get; }
-        public ISagaPersister SagaStorage { get; }
-        public ISynchronizedStorage SynchronizedStorage { get; }
-        public ISynchronizedStorageAdapter SynchronizedStorageAdapter { get; }
+        public bool SupportsDtc => false;
+        public bool SupportsOutbox => false;
+        public bool SupportsFinders => true;
+        public bool SupportsSubscriptions => true;
+        public bool SupportsTimeouts => true;
+        public bool SupportsPessimisticConcurrency => false;
+
+        public ISagaIdGenerator SagaIdGenerator { get; set; }
+        public ISagaPersister SagaStorage { get; set; }
+        public ISynchronizedStorage SynchronizedStorage { get; set; }
+        public ISynchronizedStorageAdapter SynchronizedStorageAdapter { get; set; }
         public IOutboxStorage OutboxStorage { get; }
+
         public Task Configure()
         {
-            throw new NotImplementedException();
+            var connectionString = GetEnvConfiguredConnectionStringForPersistence();
+            SagaIdGenerator = new DefaultSagaIdGenerator();
+            SagaStorage = new AzureSagaPersister(connectionString, true);
+            SynchronizedStorage = new NoOpSynchronizedStorage();
+            SynchronizedStorageAdapter = new NoOpSynchronizedStorageAdapter();
+            return Task.CompletedTask;
         }
 
         public Task Cleanup()
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
+        }
+
+        static string GetEnvConfiguredConnectionStringForPersistence()
+        {
+            var environmentVartiableName = "AzureStoragePersistence_ConnectionString";
+            var connectionString = GetEnvironmentVariable(environmentVartiableName);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new Exception($"Oh no! We couldn't find an environment variable '{environmentVartiableName}' with Azure Storage connection string.");
+            }
+
+            return connectionString;
+        }
+
+        static string GetEnvironmentVariable(string variable)
+        {
+            var candidate = Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.User);
+            return string.IsNullOrWhiteSpace(candidate) ? Environment.GetEnvironmentVariable(variable) : candidate;
         }
     }
 }
