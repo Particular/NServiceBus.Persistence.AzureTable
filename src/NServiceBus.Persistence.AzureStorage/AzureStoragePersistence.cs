ea1765ca2d3700f7d9a356fd08953be849bc3247
danielmarbach <daniel.marbach@openplace.net>
2020-10-21 18:01:04 +0200

    Enable outbox support on persistence

--- src/NServiceBus.Persistence.AzureStorage/AzureStoragePersistence.cs
+++ src/NServiceBus.Persistence.AzureStorage/AzureStoragePersistence.cs
@@ -2,6 +2,7 @@ namespace NServiceBus
 {
     using Features;
     using Persistence;
+    using Persistence.AzureStorage;
 
     /// <summary></summary>
     public class AzureStoragePersistence : PersistenceDefinition
@@ -9,6 +10,7 @@ namespace NServiceBus
         AzureStoragePersistence()
         {
             Supports<StorageType.Sagas>(s => s.EnableFeatureByDefault<AzureStorageSagaPersistence>());
+            Supports<StorageType.Outbox>(s => s.EnableFeatureByDefault<OutboxStorage>());
             Supports<StorageType.Subscriptions>(s => s.EnableFeatureByDefault<AzureStorageSubscriptionPersistence>());
         }
     }
