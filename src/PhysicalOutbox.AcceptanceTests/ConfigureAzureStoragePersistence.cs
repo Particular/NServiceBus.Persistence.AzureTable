8ccb46bac15d4a85b582db9478655dba1cba6ada
danielmarbach <daniel.marbach@openplace.net>
2020-10-27 09:06:40 +0100

    Physical outbox tests

---
+++ src/PhysicalOutbox.AcceptanceTests/ConfigureAzureStoragePersistence.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.AcceptanceTests;
+using NServiceBus.AcceptanceTests.Sagas;
+using NServiceBus.Persistence.AzureStorage.Testing;
+using NServiceBus.Pipeline;
+using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
+
+public class ConfigureEndpointAzureStoragePersistence : IConfigureEndpointTestExecution
+{
+    static string ConnectionString => Utilities.GetEnvConfiguredConnectionStringForPersistence();
+
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+    {
+        var persistence = configuration.UsePersistence<AzureStoragePersistence>();
+        persistence.ConnectionString(ConnectionString);
+
+        var recoverabilitySettings = configuration.Recoverability();
+
+        if (endpointName != Conventions.EndpointNamingConvention(typeof(When_saga_started_concurrently.ConcurrentHandlerEndpoint)))
+        {
+            recoverabilitySettings.Immediate(c => c.NumberOfRetries(1));
+        }
+
+        // This populates the partition key at the physical stage to test the conventional outbox use-case
+        configuration.Pipeline.Register(typeof(PartitionKeyProviderBehavior), "Populates the partition key");
+
+        return Task.FromResult(0);
+    }
+
+    Task IConfigureEndpointTestExecution.Cleanup()
+    {
+        return Task.FromResult(0);
+    }
+
+    class PartitionKeyProviderBehavior : Behavior<ITransportReceiveContext>
+    {
+        ScenarioContext scenarioContext;
+
+        public PartitionKeyProviderBehavior(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
+        public override Task Invoke(ITransportReceiveContext context, Func<Task> next)
+        {
+            context.Extensions.Set(new TableEntityPartitionKey(scenarioContext.TestRunId.ToString()));
+            context.Extensions.Set(new TableInformation(SetupFixture.TableName));
+            return next();
+        }
+    }
+}
---
+++ src/PhysicalOutbox.AcceptanceTests/TestSuiteConstraints.cs
@@ -0,0 +1,15 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using AcceptanceTesting.Support;
+
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc { get; } = false;
+        public bool SupportsCrossQueueTransactions { get; } = true;
+        public bool SupportsNativePubSub { get; } = true;
+        public bool SupportsNativeDeferral { get; } = true;
+        public bool SupportsOutbox { get; } = true;
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAcceptanceTestingTransport(SupportsNativePubSub, SupportsNativeDeferral);
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointAzureStoragePersistence();
+    }
+}
