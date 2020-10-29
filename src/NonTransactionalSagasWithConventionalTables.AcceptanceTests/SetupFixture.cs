aadfc66124161e5ca0598971fc849815485904ae
danielmarbach <daniel.marbach@openplace.net>
2020-10-29 14:23:18 +0100

    Cleanup setup fixture

--- src/NonTransactionalSagasWithConventionalTables.AcceptanceTests/SetupFixture.cs
+++ src/NonTransactionalSagasWithConventionalTables.AcceptanceTests/SetupFixture.cs
@@ -1,9 +1,7 @@
-﻿using System.Linq;
-
-namespace NServiceBus.AcceptanceTests
+﻿namespace NServiceBus.AcceptanceTests
 {
+    using System.Linq;
     using System;
-    using System.IO;
     using System.Threading.Tasks;
     using NUnit.Framework;
     using Microsoft.Azure.Cosmos.Table;
