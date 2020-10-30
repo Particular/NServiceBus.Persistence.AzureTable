c0ab412ccc01267be5f24901977a00ffb79c1ee8
danielmarbach <daniel.marbach@openplace.net>
2020-10-30 11:30:32 +0100

    Info log when migration mode is enabled

--- src/NServiceBus.Persistence.AzureStorage/SagaPersisters/AzureStorageSagaPersistence.cs
+++ src/NServiceBus.Persistence.AzureStorage/SagaPersisters/AzureStorageSagaPersistence.cs
@@ -36,14 +36,19 @@
             var assumeSecondaryIndicesExist = context.Settings.Get<bool>(WellKnownConfigurationKeys.SagaStorageAssumeSecondaryIndicesExist);
             var assumeSecondaryKeyUsesANonEmptyRowKeySetToThePartitionKey = context.Settings.Get<bool>(WellKnownConfigurationKeys.SagaStorageAssumeSecondaryKeyUsesANonEmptyRowKeySetToThePartitionKey);
 
+            if (migrationModeEnabled)
+            {
+                Logger.Info($"The version of {nameof(AzureStoragePersistence)} uses the migration mode and will fallback to lookup correlated sages based on the secondary index if necessary.");
+            }
+
             if (assumeSecondaryIndicesExist == false)
             {
-                logger.Warn($"The version of {nameof(AzureStoragePersistence)} used is not configured to optimize sagas creation and might fall back to full table scanning to retrieve correlated sagas. It is suggested to migrate saga instances. Consult the upgrade guides for recommendations.");
+                Logger.Warn($"The version of {nameof(AzureStoragePersistence)} used is not configured to optimize sagas creation and might fall back to full table scanning to retrieve correlated sagas. It is suggested to migrate saga instances. Consult the upgrade guides for recommendations.");
             }
 
             context.Services.AddSingleton<ISagaPersister>(provider => new AzureSagaPersister(provider.GetRequiredService<IProvideCloudTableClient>(), updateSchema, migrationModeEnabled, assumeSecondaryIndicesExist, assumeSecondaryKeyUsesANonEmptyRowKeySetToThePartitionKey));
         }
 
-        static ILog logger = LogManager.GetLogger<AzureStorageSagaPersistence>();
+        static readonly ILog Logger = LogManager.GetLogger<AzureStorageSagaPersistence>();
     }
 }
