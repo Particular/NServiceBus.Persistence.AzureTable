0be8e87e86093bdaaeaa19e3b9031cb6a2f41f5f
danielmarbach <daniel.marbach@openplace.net>
2020-11-03 16:04:14 +0100

    Fix assertions

--- src/Tests/Sagas/SecondaryIndexKeyBuilderTests.cs
+++ src/Tests/Sagas/SecondaryIndexKeyBuilderTests.cs
@@ -15,7 +15,7 @@
             metadata.TryGetCorrelationProperty(out var sagaProp);
 
             var key = SecondaryIndexKeyBuilder.BuildTableKey(typeof(SagaData), new SagaCorrelationProperty(sagaProp.Name, id));
-            var expected = "Index_NServiceBus.Persistence.AzureStorage.ComponentTests.Sagas.SecondaryIndexKeyBuilderTests+SagaData_AdditionalId_\"C4D91B59-A407-4CDA-A689-60AA3C334699\"";
+            var expected = "Index_NServiceBus.Persistence.AzureTable.ComponentTests.Sagas.SecondaryIndexKeyBuilderTests+SagaData_AdditionalId_\"C4D91B59-A407-4CDA-A689-60AA3C334699\"";
             Assert.AreEqual(expected, key.PartitionKey);
             Assert.AreEqual(string.Empty, key.RowKey);
         }
