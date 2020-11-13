7f7e8f42990e2ca34aacf5c9ebaa84549cc7c8db
danielmarbach <daniel.marbach@openplace.net>
2020-11-13 11:50:31 +0100

    Verify SecondaryIndex behaves correctly under CosmosDB in Migration mode

---
+++ src/Tests/Sagas/SecondaryIndexTests.cs
@@ -0,0 +1,83 @@
+namespace NServiceBus.Persistence.AzureTable.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Logging;
+    using Microsoft.Azure.Cosmos.Table;
+    using Sagas;
+    using Testing;
+
+    [TestFixture("CosmosDB")]
+    public class SecondaryIndexTests
+    {
+        public SecondaryIndexTests(string tableApiType)
+        {
+            this.tableApiType = tableApiType;
+        }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            logStatements = new StringBuilder();
+
+            var account = CloudStorageAccount.Parse(ConnectionStringHelper.GetEnvConfiguredConnectionStringForPersistence(tableApiType));
+
+            client = account.CreateCloudTableClient();
+            tableName = nameof(SecondaryIndexTests).ToLower();
+            cloudTable = client.GetTableReference(tableName);
+            await cloudTable.CreateIfNotExistsAsync();
+        }
+
+        [Test]
+        public async Task When_empty_row_key_should_log_warn_and_assume_row_key_partition_key()
+        {
+            var someId = new Guid("E57CF37C-1CBC-4B08-8C19-3FCE2FFC0451").ToString();
+
+            scope = LogManager.Use<TestingLoggerFactory>()
+                .BeginScope(new StringWriter(logStatements));
+
+            var secondaryIndex = new SecondaryIndex();
+
+            var result = await secondaryIndex.FindSagaId<TestSagaData>(cloudTable,
+                new SagaCorrelationProperty("SomeId", someId));
+
+            Assert.IsNull(result);
+            StringAssert.Contains("Trying to retrieve the secondary index entry with PartitionKey = 'Index_NServiceBus.Persistence.AzureTable.Tests.SecondaryIndexTests+TestSagaData_SomeId_\"e57cf37c-1cbc-4b08-8c19-3fce2ffc0451\"' and RowKey = 'string.Empty' failed", logStatements.ToString());
+        }
+
+        [Test]
+        public async Task When_opt_in_for_non_empty_row_key_should_not_log_warn()
+        {
+            var someId = new Guid("E57CF37C-1CBC-4B08-8C19-3FCE2FFC0451").ToString();
+
+            scope = LogManager.Use<TestingLoggerFactory>()
+                .BeginScope(new StringWriter(logStatements));
+
+            var secondaryIndex = new SecondaryIndex(assumeSecondaryKeyUsesANonEmptyRowKeySetToThePartitionKey: true);
+
+            var result = await secondaryIndex.FindSagaId<TestSagaData>(cloudTable,
+                new SagaCorrelationProperty("SomeId", someId));
+
+            Assert.IsNull(result);
+            Assert.IsEmpty(logStatements.ToString());
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            scope.Dispose();
+        }
+
+        class TestSagaData : ContainSagaData {}
+
+        StringBuilder logStatements;
+        IDisposable scope;
+        private CloudTable cloudTable;
+        private CloudTableClient client;
+        private string tableName;
+        private string tableApiType;
+    }
+}
