24601dfef853689561478103b34615be8e7d6988
danielmarbach <daniel.marbach@openplace.net>
2020-11-18 16:58:05 +0100

    Rename previous to Release_2x

--- src/Compatibility.StorageTable.AcceptanceTests/When_migrated_saga_completed.cs
+++ src/Compatibility.StorageTable.AcceptanceTests/When_migrated_saga_completed.cs
@@ -7,7 +7,7 @@ namespace NServiceBus.AcceptanceTests
     using EndpointTemplates;
     using NUnit.Framework;
     using Extensibility;
-    using Persistence.AzureTable.Previous;
+    using Persistence.AzureTable.Release_2x;
     using Sagas;
 
     public class When_migrated_saga_completed : CompatibilityAcceptanceTest
