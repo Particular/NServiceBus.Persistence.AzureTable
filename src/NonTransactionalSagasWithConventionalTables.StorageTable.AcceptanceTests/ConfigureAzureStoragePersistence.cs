1800ab3366ba3e1752e810730501a9485edab88a
danielmarbach <daniel.marbach@openplace.net>
2020-11-27 16:19:55 +0100

    Use the prefix backdoor

--- src/NonTransactionalSagasWithConventionalTables.StorageTable.AcceptanceTests/ConfigureAzureStoragePersistence.cs
+++ src/NonTransactionalSagasWithConventionalTables.StorageTable.AcceptanceTests/ConfigureAzureStoragePersistence.cs
@@ -3,6 +3,7 @@ using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
 using NServiceBus.AcceptanceTests;
 using NServiceBus.AcceptanceTests.Sagas;
+using NServiceBus.Configuration.AdvancedExtensibility;
 using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
 
 public class ConfigureEndpointAzureTablePersistence : IConfigureEndpointTestExecution
@@ -10,6 +11,9 @@ public class ConfigureEndpointAzureTablePersistence : IConfigureEndpointTestExec
     public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
     {
         var persistence = configuration.UsePersistence<AzureTablePersistence, StorageType.Sagas>();
+        // backdoor for testing
+        persistence.GetSettings().Set("AzureSagaStorage.ConventionalTablePrefix", SetupFixture.TablePrefix);
+
         persistence.UseCloudTableClient(SetupFixture.TableClient);
 
         persistence.Compatibility().DisableSecondaryKeyLookupForSagasCorrelatedByProperties();
