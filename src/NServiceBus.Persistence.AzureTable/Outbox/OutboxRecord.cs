e2777f77050b2338ea039cbc5eb041a78224360a
Tomek Masternak <tomasz.masternak@particular.net>
2021-08-31 11:28:46 +0200

    using custom json converter to handle readonlymemory for outbox record body

--- src/NServiceBus.Persistence.AzureTable/Outbox/OutboxRecord.cs
+++ src/NServiceBus.Persistence.AzureTable/Outbox/OutboxRecord.cs
@@ -3,13 +3,14 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Runtime.InteropServices;
     using Microsoft.Azure.Cosmos.Table;
     using Newtonsoft.Json;
     using Outbox;
 
     class OutboxRecord : TableEntity
     {
+        static ReadOnlyMemoryConverter ReadOnlyMemoryConverter = new ReadOnlyMemoryConverter();
+
         // ignoring this property to avoid double storing and clashing with Cosmos Id property.
         [IgnoreProperty]
         public string Id
@@ -25,39 +26,40 @@
         public string DispatchedAt { get; set; }
 
         // ignoring this property because we are custom serializing the operations into TransportOperations and deserializing it back
-        [IgnoreProperty]
-        public TransportOperation[] Operations { get; set; } = Array.Empty<TransportOperation>();
+        [IgnoreProperty] public TransportOperation[] Operations { get; set; } = Array.Empty<TransportOperation>();
 
         public override IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
         {
-            TransportOperations = JsonConvert.SerializeObject(
-                Operations.Select(transportOperation =>
-                {
-                    byte[] body = null;
-                    if (MemoryMarshal.TryGetArray(transportOperation.Body, out var bodySegment)) //if true this does not make an allocation
-                    {
-                        body = bodySegment.Array;
-                    }
-                    else
-                    {
-                        body = transportOperation.Body.ToArray();
-                    }
-                    return new StorageTransportOperation()
-                    {
-                        MessageId = transportOperation.MessageId,
-                        Body = body,
-                        Options = transportOperation.Options,
-                        Headers = transportOperation.Headers
-                    };
-                }), Formatting.Indented);
+            TransportOperations = SerializeTransportOperations(Operations);
             return base.WriteEntity(operationContext);
         }
 
-        public override void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        internal static StorageTransportOperation[] DeserializeTransportOperations(string transportOperations)
+        {
+            return JsonConvert.DeserializeObject<StorageTransportOperation[]>(transportOperations, ReadOnlyMemoryConverter);
+        }
+
+        internal static string SerializeTransportOperations(TransportOperation[] transportOperations)
+        {
+            return JsonConvert.SerializeObject(
+                transportOperations.Select(transportOperation => new StorageTransportOperation()
+                {
+                    MessageId = transportOperation.MessageId,
+                    Body = transportOperation.Body,
+                    Options = transportOperation.Options,
+                    Headers = transportOperation.Headers
+                }), Formatting.Indented, ReadOnlyMemoryConverter);
+        }
+
+        public override void ReadEntity(IDictionary<string, EntityProperty> properties,
+            OperationContext operationContext)
         {
             base.ReadEntity(properties, operationContext);
-            var storageOperations = JsonConvert.DeserializeObject<StorageTransportOperation[]>(TransportOperations);
-            Operations = storageOperations.Select(op => new TransportOperation(op.MessageId, new Transport.DispatchProperties(op.Options), op.Body, op.Headers)).ToArray();
+            var storageOperations = DeserializeTransportOperations(TransportOperations);
+            Operations = storageOperations.Select(op =>
+                    new TransportOperation(op.MessageId, new Transport.DispatchProperties(op.Options), op.Body,
+                        op.Headers))
+                .ToArray();
         }
 
         public void SetAsDispatched()
@@ -67,11 +69,11 @@
             DispatchedAt = DateTimeOffsetHelper.ToWireFormattedString(DateTimeOffset.UtcNow);
         }
 
-        class StorageTransportOperation
+        internal class StorageTransportOperation
         {
             public string MessageId { get; set; }
             public Dictionary<string, string> Options { get; set; }
-            public byte[] Body { get; set; }
+            public ReadOnlyMemory<byte> Body { get; set; }
             public Dictionary<string, string> Headers { get; set; }
         }
     }
