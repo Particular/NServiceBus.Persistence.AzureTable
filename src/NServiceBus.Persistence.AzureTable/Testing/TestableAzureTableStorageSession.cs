5d35992203835dab0e9862347aea42e26e714d9c
lailabougria <laila.bougria@particular.net>
2022-05-10 15:03:57 +0200

    apply operation to the batch
    Co-authored-by: Daniel Marbach <daniel.marbach@particular.net>

--- src/NServiceBus.Persistence.AzureTable/Testing/TestableAzureTableStorageSession.cs
+++ src/NServiceBus.Persistence.AzureTable/Testing/TestableAzureTableStorageSession.cs
@@ -23,10 +23,7 @@
 
         ContextBag IWorkWithSharedTransactionalBatch.CurrentContextBag { get; set; }
 
-        void IWorkWithSharedTransactionalBatch.Add(Operation operation)
-        {
-
-        }
+        void IWorkWithSharedTransactionalBatch.Add(Operation operation) => operation.Apply(Batch);
 
         /// <summary>
         /// The cloud table to be used.
