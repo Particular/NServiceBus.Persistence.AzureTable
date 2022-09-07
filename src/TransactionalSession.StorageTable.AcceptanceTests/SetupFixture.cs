31af7ed587bd19346b3fbfd942c93846aa1c9e04
Daniel Marbach <daniel.marbach@nservicebus.com>
2022-09-07 12:13:37 +0200

    Implement TransactionalSession as part of the persistence (#613)
    * Implement TransactionalSession as part of the persistence
    
    * Make transactional session part of the persistence seam
    
    * ApiApprovals
    
    * Add TransactionalSession to scanning too
    
    * Update src/NServiceBus.Persistence.AzureTable.TransactionalSession/AzureTableOpenSessionOptions.cs
    
    Co-authored-by: Tomasz Masternak <tomasz.masternak@particular.net>
    
    * Beta5
    
    * Move towards making tx session atts its own concept
    
    * Activate base feature too
    
    * Include missing files :facepalm:
    
    * Remove redundant code
    
    * Adjust to latest transactional session changes
    
    * Take an explicit dependency
    
    * Bump RC3
    
    * Calling base setup
    
    Co-authored-by: Tomasz Masternak <tomasz.masternak@particular.net>

---
+++ src/TransactionalSession.StorageTable.AcceptanceTests/SetupFixture.cs
@@ -0,0 +1,33 @@
+﻿namespace NServiceBus.TransactionalSession.AcceptanceTests
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using Microsoft.Azure.Cosmos.Table;
+    using Testing;
+
+    [SetUpFixture]
+    public class SetupFixture
+    {
+        [OneTimeSetUp]
+        public async Task OneTimeSetUp()
+        {
+            var connectionString = this.GetEnvConfiguredConnectionStringByCallerConvention();
+
+            TableName = $"{Path.GetFileNameWithoutExtension(Path.GetTempFileName())}{DateTime.UtcNow.Ticks}".ToLowerInvariant();
+
+            var account = CloudStorageAccount.Parse(connectionString);
+            TableClient = account.CreateCloudTableClient();
+            Table = TableClient.GetTableReference(TableName);
+            await Table.CreateIfNotExistsAsync();
+        }
+
+        [OneTimeTearDown]
+        public Task OneTimeTearDown() => Table.DeleteIfExistsAsync();
+
+        public static string TableName;
+        public static CloudTableClient TableClient;
+        public static CloudTable Table;
+    }
+}
---
+++ src/TransactionalSession.StorageTable.AcceptanceTests/When_using_transactional_session.cs
@@ -0,0 +1,226 @@
+namespace NServiceBus.TransactionalSession.AcceptanceTests
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using ObjectBuilder;
+    using Microsoft.Azure.Cosmos.Table;
+    using NUnit.Framework;
+    using Persistence.AzureTable;
+    using System.Linq;
+
+    public class When_using_transactional_session : NServiceBusAcceptanceTest
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task Should_send_messages_and_store_document_in_synchronized_session_on_transactional_session_commit(bool outboxEnabled)
+        {
+            var entityRowId = Guid.NewGuid().ToString();
+
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<AnEndpoint>(s => s.When(async (_, ctx) =>
+                {
+                    using var scope = ctx.Builder.CreateChildBuilder();
+                    using var transactionalSession = scope.Build<ITransactionalSession>();
+                    await transactionalSession.Open(new AzureTableOpenSessionOptions(new TableEntityPartitionKey(ctx.TestRunId.ToString()), new TableInformation(SetupFixture.TableName)));
+
+                    await transactionalSession.SendLocal(new SampleMessage());
+
+                    var storageSession = transactionalSession.SynchronizedStorageSession.AzureTablePersistenceSession();
+
+                    var entity = new MyTableEntity
+                    {
+                        RowKey = entityRowId,
+                        PartitionKey = ctx.TestRunId.ToString(),
+                        Data = "MyCustomData"
+                    };
+
+                    storageSession.Batch.Add(TableOperation.Insert(entity));
+
+                    await transactionalSession.Commit().ConfigureAwait(false);
+                }))
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            var query = new TableQuery<DynamicTableEntity>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, context.TestRunId.ToString()))
+                .Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, entityRowId));
+
+            var tableEntity = SetupFixture.Table.ExecuteQuery(query).FirstOrDefault();
+
+            Assert.IsNotNull(tableEntity);
+            Assert.AreEqual(tableEntity.Properties["Data"].StringValue, "MyCustomData");
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task Should_send_messages_and_store_document_in_azuretable_session_on_transactional_session_commit(bool outboxEnabled)
+        {
+            var entityRowId = Guid.NewGuid().ToString();
+
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<AnEndpoint>(s => s.When(async (_, ctx) =>
+                {
+                    using var scope = ctx.Builder.CreateChildBuilder();
+                    using var transactionalSession = scope.Build<ITransactionalSession>();
+                    await transactionalSession.Open(new AzureTableOpenSessionOptions(new TableEntityPartitionKey(ctx.TestRunId.ToString()), new TableInformation(SetupFixture.TableName)));
+
+                    await transactionalSession.SendLocal(new SampleMessage());
+
+                    var storageSession = scope.Build<IAzureTableStorageSession>();
+
+                    var entity = new MyTableEntity
+                    {
+                        RowKey = entityRowId,
+                        PartitionKey = ctx.TestRunId.ToString(),
+                        Data = "MyCustomData"
+                    };
+
+                    storageSession.Batch.Add(TableOperation.Insert(entity));
+
+                    await transactionalSession.Commit(CancellationToken.None).ConfigureAwait(false);
+                }))
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            var query = new TableQuery<DynamicTableEntity>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, context.TestRunId.ToString()))
+                .Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, entityRowId));
+
+            var tableEntity = SetupFixture.Table.ExecuteQuery(query).FirstOrDefault();
+
+            Assert.IsNotNull(tableEntity);
+            Assert.AreEqual(tableEntity.Properties["Data"].StringValue, "MyCustomData");
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task Should_not_send_messages_if_session_is_not_committed(bool outboxEnabled)
+        {
+            var entityRowId = Guid.NewGuid().ToString();
+
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<AnEndpoint>(s => s.When(async (statelessSession, ctx) =>
+                {
+                    using (var scope = ctx.Builder.CreateChildBuilder())
+                    using (var transactionalSession = scope.Build<ITransactionalSession>())
+                    {
+                        await transactionalSession.Open(new AzureTableOpenSessionOptions(new TableEntityPartitionKey(ctx.TestRunId.ToString()), new TableInformation(SetupFixture.TableName)));
+
+                        await transactionalSession.SendLocal(new SampleMessage());
+
+                        var storageSession = transactionalSession.SynchronizedStorageSession.AzureTablePersistenceSession();
+
+                        var entity = new MyTableEntity
+                        {
+                            RowKey = entityRowId,
+                            PartitionKey = ctx.TestRunId.ToString(),
+                            Data = "MyCustomData"
+                        };
+
+                        storageSession.Batch.Add(TableOperation.Insert(entity));
+                    }
+
+                    //Send immediately dispatched message to finish the test
+                    await statelessSession.SendLocal(new CompleteTestMessage());
+                }))
+                .Done(c => c.CompleteMessageReceived)
+                .Run();
+
+            Assert.True(context.CompleteMessageReceived);
+            Assert.False(context.MessageReceived);
+
+            var query = new TableQuery<DynamicTableEntity>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, context.TestRunId.ToString()))
+                .Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, entityRowId));
+            Assert.IsEmpty(SetupFixture.Table.ExecuteQuery(query));
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task Should_send_immediate_dispatch_messages_even_if_session_is_not_committed(bool outboxEnabled)
+        {
+            var result = await Scenario.Define<Context>()
+                .WithEndpoint<AnEndpoint>(s => s.When(async (_, ctx) =>
+                {
+                    using var scope = ctx.Builder.CreateChildBuilder();
+                    using var transactionalSession = scope.Build<ITransactionalSession>();
+
+                    await transactionalSession.Open(new AzureTableOpenSessionOptions(new TableEntityPartitionKey(ctx.TestRunId.ToString()), new TableInformation(SetupFixture.TableName)));
+
+                    var sendOptions = new SendOptions();
+                    sendOptions.RequireImmediateDispatch();
+                    sendOptions.RouteToThisEndpoint();
+                    await transactionalSession.Send(new SampleMessage(), sendOptions);
+                }))
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            Assert.True(result.MessageReceived);
+        }
+
+        class Context : ScenarioContext, IInjectBuilder
+        {
+            public bool MessageReceived { get; set; }
+            public bool CompleteMessageReceived { get; set; }
+            public IBuilder Builder { get; set; }
+        }
+
+        class AnEndpoint : EndpointConfigurationBuilder
+        {
+            public AnEndpoint()
+            {
+                if ((bool)TestContext.CurrentContext.Test.Arguments[0]!)
+                {
+                    EndpointSetup<TransactionSessionDefaultServer>();
+                }
+                else
+                {
+                    EndpointSetup<TransactionSessionWithOutboxEndpoint>();
+                }
+            }
+
+            class SampleHandler : IHandleMessages<SampleMessage>
+            {
+                public SampleHandler(Context testContext) => this.testContext = testContext;
+
+                public Task Handle(SampleMessage message, IMessageHandlerContext context)
+                {
+                    testContext.MessageReceived = true;
+
+                    return Task.CompletedTask;
+                }
+
+                readonly Context testContext;
+            }
+
+            class CompleteTestMessageHandler : IHandleMessages<CompleteTestMessage>
+            {
+                public CompleteTestMessageHandler(Context context) => testContext = context;
+
+                public Task Handle(CompleteTestMessage message, IMessageHandlerContext context)
+                {
+                    testContext.CompleteMessageReceived = true;
+
+                    return Task.CompletedTask;
+                }
+
+                readonly Context testContext;
+            }
+        }
+
+        class SampleMessage : ICommand
+        {
+        }
+
+        class CompleteTestMessage : ICommand
+        {
+        }
+
+        public class MyTableEntity : TableEntity
+        {
+            public string Data { get; set; }
+        }
+    }
+}
