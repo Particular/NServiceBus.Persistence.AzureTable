4586cde3312178749e24ab2b414df673623b42eb
danielmarbach <daniel.marbach@openplace.net>
2022-11-09 17:14:49 +0100

    Make CompatibilityAcceptanceTest and switch to new SDK

--- src/Compatibility.StorageTable.AcceptanceTests/CompatibilityAcceptanceTest.cs
+++ src/Compatibility.StorageTable.AcceptanceTests/CompatibilityAcceptanceTest.cs
@@ -1,32 +1,73 @@
 namespace NServiceBus.AcceptanceTests
 {
+    using System;
     using System.Linq;
     using System.Net;
+    using System.Reflection;
     using System.Threading.Tasks;
-    using Microsoft.Azure.Cosmos.Table;
+    using Azure;
+    using Azure.Data.Tables;
     using Persistence.AzureTable.Release_2x;
-    using Testing;
+    using Sagas;
 
-    public class CompatibilityAcceptanceTest : NServiceBusAcceptanceTest
+    public abstract class CompatibilityAcceptanceTest : NServiceBusAcceptanceTest
     {
-        public CompatibilityAcceptanceTest()
+        /// <summary>
+        /// Stores the saga table entity in a format that mimics the 2.4.x version of the saga persistence.
+        /// </summary>
+        /// <param name="sagaData"></param>
+        /// <param name="correlationProperty"></param>
+        /// <typeparam name="TSagaTableEntity"></typeparam>
+        /// <exception cref="InvalidOperationException"></exception>
+        protected static async Task SaveSagaInOldFormat<TSagaTableEntity>(TSagaTableEntity sagaData, SagaCorrelationProperty correlationProperty)
+            where TSagaTableEntity : SagaDataTableEntity
         {
-            PersisterUsingSecondaryIndexes = new SagaPersisterUsingSecondaryIndexes(this.GetEnvConfiguredConnectionStringByCallerConvention(), true, assumeSecondaryIndicesExist: true);
+            var tableServiceClient = SetupFixture.TableServiceClient;
+
+            if (typeof(TSagaTableEntity).GetCustomAttribute(typeof(SagaEntityTypeAttribute)) is not SagaEntityTypeAttribute sagaEntityTypeAttribute)
+            {
+                throw new InvalidOperationException("Specify SagaEntityTypeAttribute on the entity type");
+            }
+
+            var sagaDataType = sagaEntityTypeAttribute.SagaEntityType;
+            var tableClient = tableServiceClient.GetTableClient($"{SetupFixture.TablePrefix}{sagaDataType.Name}");
+
+            // Given this code doesn't need to deal with race conditions like the 2.4.x version the order of operations
+            // doesn't matter and the third step of remove the data of the primary from the 2nd index. is executed in one
+            // operation. The original order was
+            // The following operations have to be executed sequentially:
+            // 1) insert the 2nd index, containing the primary saga data (just in case of a failure)
+            // 2) insert the primary saga data in its row, storing the identifier of the secondary index as well (for completions)
+            // 3) remove the data of the primary from the 2nd index. It will be no longer needed
+
+            var key = SecondaryIndexKeyBuilder.BuildTableKey(sagaDataType, correlationProperty);
+
+            var newSecondaryIndexEntity = new SecondaryIndexTableEntity
+            {
+                SagaId = new Guid(sagaData.RowKey),
+                PartitionKey = key.PartitionKey,
+                RowKey = key.RowKey,
+                ETag = ETag.All
+            };
+
+            sagaData.NServiceBus_2ndIndexKey = key.ToString();
+
+            await tableClient.UpsertEntityAsync(sagaData);
+            await tableClient.UpsertEntityAsync(newSecondaryIndexEntity);
         }
 
-        protected SagaPersisterUsingSecondaryIndexes PersisterUsingSecondaryIndexes { get; }
-
-        protected static async Task ReplaceEntity<TSagaData>(DynamicTableEntity entity)
+        protected static async Task ReplaceEntity<TSagaData>(ITableEntity entity)
         {
-            var table = SetupFixture.TableClient.GetTableReference($"{SetupFixture.TablePrefix}{typeof(TSagaData).Name}");
+            var tableServiceClient = SetupFixture.TableServiceClient;
+            var tableClient = tableServiceClient.GetTableClient($"{SetupFixture.TablePrefix}{typeof(TSagaData).Name}");
 
             try
             {
-                await table.ExecuteAsync(TableOperation.InsertOrReplace(entity));
+                await tableClient.UpsertEntityAsync(entity);
             }
-            catch (StorageException e)
+            catch (RequestFailedException e)
             {
-                if (e.RequestInformation.HttpStatusCode == (int)HttpStatusCode.NotFound)
+                if (e.Status == (int)HttpStatusCode.NotFound)
                 {
                     return;
                 }
@@ -35,17 +76,18 @@ namespace NServiceBus.AcceptanceTests
             }
         }
 
-        protected static async Task DeleteEntity<TSagaData>(DynamicTableEntity entity)
+        protected static async Task DeleteEntity<TSagaData>(ITableEntity entity)
         {
-            var table = SetupFixture.TableClient.GetTableReference($"{SetupFixture.TablePrefix}{typeof(TSagaData).Name}");
+            var tableServiceClient = SetupFixture.TableServiceClient;
+            var tableClient = tableServiceClient.GetTableClient($"{SetupFixture.TablePrefix}{typeof(TSagaData).Name}");
 
             try
             {
-                await table.ExecuteAsync(TableOperation.Delete(entity));
+                await tableClient.DeleteEntityAsync(entity.PartitionKey, entity.RowKey);
             }
-            catch (StorageException e)
+            catch (RequestFailedException e)
             {
-                if (e.RequestInformation.HttpStatusCode == (int)HttpStatusCode.NotFound)
+                if (e.Status == (int)HttpStatusCode.NotFound)
                 {
                     return;
                 }
@@ -54,21 +96,22 @@ namespace NServiceBus.AcceptanceTests
             }
         }
 
-        protected static DynamicTableEntity GetByPartitionKey<TSagaData>(string partitionKey)
+        protected static async Task<TableEntity> GetByPartitionKey<TSagaData>(string partitionKey)
         {
-            var table = SetupFixture.TableClient.GetTableReference($"{SetupFixture.TablePrefix}{typeof(TSagaData).Name}");
-
-            // table scan but still probably the easiest way to do it, otherwise we would have to take the partition key into account which complicates things because this test is shared
-            var query = new TableQuery<DynamicTableEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionKey));
+            var tableServiceClient = SetupFixture.TableServiceClient;
+            var tableClient = tableServiceClient.GetTableClient($"{SetupFixture.TablePrefix}{typeof(TSagaData).Name}");
 
             try
             {
-                var tableEntity = table.ExecuteQuery(query).FirstOrDefault();
-                return tableEntity;
+                // table scan but still probably the easiest way to do it, otherwise we would have to take the partition key into account which complicates things because this test is shared
+                var entities = await tableClient.QueryAsync<TableEntity>()
+                    .Where(x => x.PartitionKey == partitionKey)
+                    .ToListAsync();
+                return entities.FirstOrDefault();
             }
-            catch (StorageException e)
+            catch (RequestFailedException e)
             {
-                if (e.RequestInformation.HttpStatusCode == (int)HttpStatusCode.NotFound)
+                if (e.Status == (int)HttpStatusCode.NotFound)
                 {
                     return null;
                 }
@@ -77,21 +120,22 @@ namespace NServiceBus.AcceptanceTests
             }
         }
 
-        protected static DynamicTableEntity GetByRowKey<TSagaData>(string rowKey)
+        protected static async Task<TableEntity> GetByRowKey<TSagaData>(string rowKey)
         {
-            var table = SetupFixture.TableClient.GetTableReference($"{SetupFixture.TablePrefix}{typeof(TSagaData).Name}");
-
-            // table scan but still probably the easiest way to do it, otherwise we would have to take the partition key into account which complicates things because this test is shared
-            var query = new TableQuery<DynamicTableEntity>().Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, rowKey));
+            var tableServiceClient = SetupFixture.TableServiceClient;
+            var tableClient = tableServiceClient.GetTableClient($"{SetupFixture.TablePrefix}{typeof(TSagaData).Name}");
 
             try
             {
-                var tableEntity = table.ExecuteQuery(query).FirstOrDefault();
-                return tableEntity;
+                // table scan but still probably the easiest way to do it, otherwise we would have to take the partition key into account which complicates things because this test is shared
+                var entities = await tableClient.QueryAsync<TableEntity>()
+                    .Where(x => x.RowKey == rowKey)
+                    .ToListAsync();
+                return entities.FirstOrDefault();
             }
-            catch (StorageException e)
+            catch (RequestFailedException e)
             {
-                if (e.RequestInformation.HttpStatusCode == (int)HttpStatusCode.NotFound)
+                if (e.Status == (int)HttpStatusCode.NotFound)
                 {
                     return null;
                 }
