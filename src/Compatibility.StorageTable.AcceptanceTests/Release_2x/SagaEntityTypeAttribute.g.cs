d635d1d7f892c6b6356550fc44570ba2a3cf1d4c
danielmarbach <daniel.marbach@openplace.net>
2022-11-14 11:17:42 +0100

    Readme update

--- src/Compatibility.StorageTable.AcceptanceTests/Release_2x/SagaEntityTypeAttribute.g.cs
+++ src/Compatibility.StorageTable.AcceptanceTests/Release_2x/SagaEntityTypeAttribute.g.cs
@@ -3,7 +3,7 @@ namespace NServiceBus.Persistence.AzureTable.Release_2x
     using System;
 
     [AttributeUsage(AttributeTargets.Class)]
-    public sealed class SagaEntityTypeAttribute : Attribute
+    sealed class SagaEntityTypeAttribute : Attribute
     {
         public Type SagaEntityType { get; set; }
     }
