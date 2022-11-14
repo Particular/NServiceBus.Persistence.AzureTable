d6915fb21a80abd3c7e83298d32f83f961cfa994
danielmarbach <daniel.marbach@openplace.net>
2022-11-14 18:03:06 +0100

    Remove XML document from obsoletes

--- src/NServiceBus.Persistence.AzureTable/Obsoletes.cs
+++ src/NServiceBus.Persistence.AzureTable/Obsoletes.cs
@@ -1,26 +1,15 @@
-﻿namespace NServiceBus.Persistence.AzureTable
+﻿#pragma warning disable CS1591
+namespace NServiceBus.Persistence.AzureTable
 {
-    /// <summary>
-    /// Provides a CloudTableClient for the saga and outbox storage type via dependency injection. A custom implementation can be registered on the container and will be picked up by the persistence.
-    /// </summary>
     [ObsoleteEx(Message = "The IProvideCloudTableClient is deprecated in favor of IProvideTableServiceClient, which supports the TableServiceClient API provided by Azure.Data.Tables.", TreatAsErrorFromVersion = "5.0.0", RemoveInVersion = "6.0.0")]
     public interface IProvideCloudTableClient
     {
-        /// <summary>
-        /// The CloudTableClient to use.
-        /// </summary>
         object Client { get; }
     }
 
-    /// <summary>
-    /// Provides a CloudTableClient for the subscription storage type via dependency injection. A custom implementation can be registered on the container and will be picked up by the persistence.
-    /// </summary>
     [ObsoleteEx(Message = "The IProvideCloudTableClientForSubscriptions is deprecated in favor of IProvideTableServiceClientForSubscriptions, which supports the TableServiceClient API provided by Azure.Data.Tables.", TreatAsErrorFromVersion = "5.0.0", RemoveInVersion = "6.0.0")]
     public interface IProvideCloudTableClientForSubscriptions
     {
-        /// <summary>
-        /// The CloudTableClient to use.
-        /// </summary>
         object Client { get; }
     }
 }
@@ -31,28 +20,19 @@ namespace NServiceBus
 
     public static partial class ConfigureAzureSagaStorage
     {
-        /// <summary>
-        /// Cloud Table Client to use for Saga, Outbox and Subscription storage.
-        /// </summary>
         [ObsoleteEx(Message = "The UseCloudTableClient method is deprecated in favor of the UseTableServiceClient method, which supports the TableServiceClient API provided by Azure.Data.Tables.", TreatAsErrorFromVersion = "5.0.0", RemoveInVersion = "6.0.0")]
         public static PersistenceExtensions<AzureTablePersistence> UseCloudTableClient(
-            this PersistenceExtensions<AzureTablePersistence> config, object client)
-        {
+            this PersistenceExtensions<AzureTablePersistence> config, object client) =>
             throw new NotImplementedException();
-        }
     }
 
     public static partial class ConfigureAzureSubscriptionStorage
     {
-        /// <summary>
-        /// Cloud Table Client to use for the Subscription storage.
-        /// </summary>
         [ObsoleteEx(Message = "The UseCloudTableClient method is deprecated in favor of the UseTableServiceClient method, which supports the TableServiceClient API provided by Azure.Data.Tables.", TreatAsErrorFromVersion = "5.0.0", RemoveInVersion = "6.0.0")]
         public static PersistenceExtensions<AzureTablePersistence, StorageType.Subscriptions> UseCloudTableClient(
             this PersistenceExtensions<AzureTablePersistence, StorageType.Subscriptions> config,
-            object client)
-        {
+            object client) =>
             throw new NotImplementedException();
-        }
     }
 }
+#pragma warning restore CS1591
