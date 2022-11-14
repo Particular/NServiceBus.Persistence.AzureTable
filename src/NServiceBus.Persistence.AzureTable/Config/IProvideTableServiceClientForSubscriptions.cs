aff3d56df10ba4cce38c5fb059b94c699a9d52ed
danielmarbach <daniel.marbach@openplace.net>
2022-11-14 15:52:45 +0100

    Link to client

--- src/NServiceBus.Persistence.AzureTable/Config/IProvideTableServiceClientForSubscriptions.cs
+++ src/NServiceBus.Persistence.AzureTable/Config/IProvideTableServiceClientForSubscriptions.cs
@@ -3,7 +3,7 @@
     using Azure.Data.Tables;
 
     /// <summary>
-    /// Provides a TableServiceClient for the subscription storage type via dependency injection. A custom implementation can be registered on the container and will be picked up by the persistence.
+    /// Provides a <see cref="Azure.Data.Tables.TableServiceClient"/> for the subscription storage type via dependency injection. A custom implementation can be registered on the container and will be picked up by the persistence.
     /// </summary>
     public interface IProvideTableServiceClientForSubscriptions
     {
