09c6247b1bbc249b10571a081313583a81e51fa7
Tamara Rivera <tamita.rivera@gmail.com>
2025-06-05 17:53:07 -0700

    Adding to the obsolete all APIs related to compatibility

--- src/NServiceBus.Persistence.AzureTable/obsoletes-v7.cs
+++ src/NServiceBus.Persistence.AzureTable/obsoletes-v7.cs
@@ -3,9 +3,23 @@
 namespace NServiceBus;
 
 using System;
+using Configuration.AdvancedExtensibility;
+using Settings;
 
-public partial class CompatibilitySettings
+public partial class ConfigureAzureSagaStorage
 {
+    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
+        TreatAsErrorFromVersion = "7.0")]
+    public static CompatibilitySettings Compatibility(this PersistenceExtensions<AzureTablePersistence, StorageType.Sagas> config) => throw new NotImplementedException();
+}
+
+[ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
+    TreatAsErrorFromVersion = "7.0")]
+public class CompatibilitySettings : ExposeSettings
+{
+    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
+        TreatAsErrorFromVersion = "7.0")]
+    internal CompatibilitySettings(SettingsHolder settings) : base(settings) => throw new NotImplementedException();
 
     [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
         TreatAsErrorFromVersion = "7.0")]
