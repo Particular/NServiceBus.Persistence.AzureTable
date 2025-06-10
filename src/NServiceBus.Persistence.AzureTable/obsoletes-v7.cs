91ce19b19dae8971c1fcd6a3b670c9841cfce952
Brandon Ording <bording@gmail.com>
2025-06-10 11:55:23 -0400

    Formatting

--- src/NServiceBus.Persistence.AzureTable/obsoletes-v7.cs
+++ src/NServiceBus.Persistence.AzureTable/obsoletes-v7.cs
@@ -8,29 +8,23 @@ using Settings;
 
 public partial class ConfigureAzureSagaStorage
 {
-    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
-        TreatAsErrorFromVersion = "7.0")]
+    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0", TreatAsErrorFromVersion = "7.0")]
     public static CompatibilitySettings Compatibility(this PersistenceExtensions<AzureTablePersistence, StorageType.Sagas> config) => throw new NotImplementedException();
 }
 
-[ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
-    TreatAsErrorFromVersion = "7.0")]
+[ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0", TreatAsErrorFromVersion = "7.0")]
 public class CompatibilitySettings : ExposeSettings
 {
-    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
-        TreatAsErrorFromVersion = "7.0")]
+    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0", TreatAsErrorFromVersion = "7.0")]
     internal CompatibilitySettings(SettingsHolder settings) : base(settings) => throw new NotImplementedException();
 
-    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
-        TreatAsErrorFromVersion = "7.0")]
+    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0", TreatAsErrorFromVersion = "7.0")]
     public void EnableSecondaryKeyLookupForSagasCorrelatedByProperties() => throw new NotImplementedException();
 
-    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
-        TreatAsErrorFromVersion = "7.0")]
+    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0", TreatAsErrorFromVersion = "7.0")]
     public void AllowSecondaryKeyLookupToFallbackToFullTableScan() => throw new NotImplementedException();
 
-    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0",
-        TreatAsErrorFromVersion = "7.0")]
+    [ObsoleteEx(Message = "Compatibility mode is deprecated.", RemoveInVersion = "8.0", TreatAsErrorFromVersion = "7.0")]
     public void AssumeSecondaryKeyUsesANonEmptyRowKeySetToThePartitionKey() => throw new NotImplementedException();
 }
 
