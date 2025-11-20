3d4a591f15735f3be708ff39ce5a818e0501ae72
David Boike <david.boike@gmail.com>
2025-11-20 13:14:26 -0600

    One more shot at delay with output logging

--- src/LogicalOutbox.StorageTable.AcceptanceTests/SetupFixture.cs
+++ src/LogicalOutbox.StorageTable.AcceptanceTests/SetupFixture.cs
@@ -6,7 +6,6 @@
     using System.Threading.Tasks;
     using Azure;
     using Azure.Data.Tables;
-    using Azure.Data.Tables.Models;
     using NUnit.Framework;
     using Testing;
 
@@ -20,14 +19,20 @@
 
             TableName = $"{Path.GetFileNameWithoutExtension(Path.GetTempFileName())}{DateTime.UtcNow.Ticks}".ToLowerInvariant();
 
-            for (var i = 0; i < 12; i++)
+            const int minutes = 5;
+
+            for (var i = 0; i < minutes * 4; i++)
             {
                 try
                 {
                     TableServiceClient = new TableServiceClient(ConnectionString);
                     TableClient = TableServiceClient.GetTableClient(TableName);
                     var response = await TableClient.CreateIfNotExistsAsync();
+                    await TestContext.Out.WriteLineAsync($"OneTimeSetUp created table {TableName} to test Cosmos DB readiness");
                     Assert.That(response.Value, Is.Not.Null);
+
+                    await TestContext.Out.WriteLineAsync("Waiting an additional 15s for Comsos DB to be available anyway");
+                    await Task.Delay(TimeSpan.FromSeconds(15));
                     return;
                 }
                 catch (RequestFailedException e)
@@ -35,7 +40,7 @@
                     var response = e.GetRawResponse();
                     if (response?.Status == 403)
                     {
-                        Console.WriteLine($"Create table failed with Status 403 ({response.ReasonPhrase}), will wait 15s up to 3m");
+                        await TestContext.Out.WriteLineAsync($"Create table failed with Status 403 ({response.ReasonPhrase}), will wait 15s up to {minutes}m");
                         await Task.Delay(TimeSpan.FromSeconds(15));
                     }
                     else
